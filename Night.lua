1cfc74b9acbaee92964482dc7609dbf9f75a7feb
anonym <242286542+NightSyste@users.noreply.github.com>
2026-08-31 15:05:27 +0200

    Update Night.lua

--- Night.lua
+++ Night.lua
@@ -57,7 +57,7 @@ local ACCENT_SOFT  = Library.AccentSoft
 -- Wird für: Fenster-Icon (oben links), Loader-Icon beim Laden
 -- und das kleine Icon verwendet, das erscheint, wenn die UI
 -- geschlossen/minimiert wird.
-Library.FixedIconId = "rbxassetid://111145155979089"
+Library.FixedIconId = "rbxassetid://123815022027998"
 
 local function PackColor(Color)
 	return {R = Color.R * 255, G = Color.G * 255, B = Color.B * 255}
